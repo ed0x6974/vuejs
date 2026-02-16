d530e43611515381ca1d7cbf1b05b63a7288ca9d
ed0x6974 <edlayweb@gmail.com>
2026-02-16 15:26:20 +0100

    [refactoring] allow ref() to accept both primitives and objects as a params

--- script.js
+++ script.js
@@ -1,117 +1,129 @@
 function useVue() {
-    let caller = null; 
-    const listeners2 = [];
+  let calledEffect = null;
 
-    function ref(state) {
-        const listeners = [];
+  function ref(rawValue) {
+    const isObject = (val) => val !== null && typeof val === "object";
+    const listeners = [];
+    let _value = isObject(rawValue) ? reactive(rawValue) : rawValue;
 
-        return new Proxy(state, {
-            get(target, prop, receiver) {
-                if (caller) {
-                    listeners.push(caller);
-                    caller = null;
-                }
-                
-                return Reflect.get(target, prop, receiver);
-            },
-            set(target, prop, value, receiver) {
-                const result = Reflect.set(target, prop, value, receiver);
-
-                [...listeners, ...listeners2].forEach(cb => cb());
-
-                return result;
-            }
-        });
-    }
-
-    function watchEffect(cb) {
-        caller = cb;
-        cb();
-    }
-
-    function computed(cb) {
-        const listeners = [];
-        let currentCaller = null;
-        let value = cb(); // но сначала нужно вызвать cb и определить от чего он зависит, и туда его подписать
-
-        const setValue = (bool) => {
-            if (bool !== value) {
-                value = bool;
-                currentCaller();
-            }
-        }
-
-        const onRefUpdated = () => {
-            setValue(cb());
-        }
-
-        listeners2.push(onRefUpdated);
-
-        return {
-            get value() {
-                if (caller) {
-                    currentCaller = caller;
-                    caller = null;
-                }
-
-                return value;
-            },
-        }
+    function reactive(rawValue) {
+      return new Proxy(rawValue, {
+        get(target, key) {
+          if (calledEffect) {
+            listeners.push(calledEffect);
+            calledEffect = null;
+          }
+          return target[key];
+        },
+        set(target, key, value) {
+          target[key] = value;
+          listeners.forEach((cb) => cb());
+        },
+      });
     }
 
     return {
-        ref,
-        watchEffect,
-        computed,
-    }
-}
+      get value() {
+        if (calledEffect) {
+          listeners.push(calledEffect);
+          calledEffect = null;
+        }
 
-const {
+        return _value;
+      },
+
+      set value(newValue) {
+        _value = newValue;
+        listeners.forEach((cb) => cb());
+      },
+    };
+  }
+
+  function watchEffect(onEffect) {
+    calledEffect = onEffect;
+    onEffect();
+  }
+
+  function computed(cb) {
+    let computedEffect = null;
+    let value = null;
+
+    const setValue = (newValue) => {
+      if (newValue !== value) {
+        value = newValue;
+
+        if (computedEffect) {
+          computedEffect();
+        }
+      }
+    };
+
+    watchEffect(() => {
+      setValue(cb());
+    });
+
+    return {
+      get value() {
+        if (calledEffect) {
+          computedEffect = calledEffect;
+          calledEffect = null;
+          setValue(computedEffect());
+        }
+
+        return value;
+      },
+    };
+  }
+
+  return {
     ref,
     watchEffect,
     computed,
-} = useVue();
+  };
+}
 
-const $buttonAdd = document.getElementById('add');
-const $buttonReset = document.getElementById('reset');
-const $score = document.getElementById('score');
+const { ref, watchEffect, computed } = useVue();
+
+const $buttonAdd = document.getElementById("add");
+const $buttonReset = document.getElementById("reset");
+const $score = document.getElementById("score");
 
 const state = {
-    value: 0,
-}
+  count: 0,
+};
 
 const counterState = ref(state);
 
 const isTooBig = computed(() => {
-    debugger;
-    return counterState.value > 10
+  debugger;
+  return counterState.value.count > 10;
 });
 
 watchEffect(renderChangeColor);
 watchEffect(renderChangeValue);
 
 function renderChangeValue() {
-    console.log('html');
-    $score.innerText = `${counterState.value}`;
+  console.log("html");
+  $score.innerText = `${counterState.value.count}`;
 }
 
 function renderChangeColor() {
-    console.log('color');
-    if (isTooBig.value) {
-        $score.style.background = "green";
-    } else {
-        $score.style.background = "red";
-    }
+  console.log("color");
+  if (isTooBig.value) {
+    $score.style.background = "green";
+  } else {
+    $score.style.background = "red";
+  }
 }
 
-$buttonAdd.addEventListener('click', () => {
-    counterState.value++;
-})
+$buttonAdd.addEventListener("click", () => {
+  counterState.value.count++;
+});
 
-$buttonReset.addEventListener('click', () => {
-    counterState.value = 0;
-})
+$buttonReset.addEventListener("click", () => {
+  counterState.value.count = 0;
+});
 
 setInterval(() => {
-    counterState.value += 5;
+  counterState.value.count += 5;
 }, 3000);
